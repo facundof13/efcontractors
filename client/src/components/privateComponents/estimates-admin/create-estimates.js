b0118dffbd9b5fa4ebc5dba5adfed7706500e721
Facundo Figueroa <facundof13@gmail.com>
2019-07-15 18:40:36 -0400

    estimates updates

--- client/src/components/privateComponents/estimates-admin/create-estimates.js
+++ client/src/components/privateComponents/estimates-admin/create-estimates.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
-import { TextField, Button, Checkbox, FormControlLabel } from "@material-ui/core";
+import {
+  TextField,
+  Button,
+  Checkbox,
+  FormControlLabel
+} from "@material-ui/core";
 import Axios from "axios";
-
+import ItemField from "./estimates-items-field";
 export default class CreateEstimate extends React.Component {
   constructor() {
     super();
-    this.state = {services: [],}
+    this.state = { services: [], numItems: 1, itemsField: []};
     this.handleChange = this.handleChange.bind(this);
+    this.addItem = this.addItem.bind(this);    
   }
 
   componentDidMount() {
-    this.getServices()
+    this.getServices();
+    var items = [];
+    for (let i = 0; i < this.state.numItems; i++) {
+      items.push(<ItemField key={i} removeItem={this.removeItem} num ={i} services={this.state.services} />);
+    }
+    this.setState({itemsField: items})
   }
-
 
   handleChange(event) {
     this.setState({
@@ -23,98 +33,89 @@ export default class CreateEstimate extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    document.getElementById('create-form').reset()
+    document.getElementById("create-form").reset();
   }
 
   getServices() {
-    Axios.get('/admin/invoiceServices')
-    .then(res => {
-      this.setState({services: res.data})
-      console.log(this.state.services)
-    })
+    Axios.get("/admin/invoiceServices").then(res => {
+      this.setState({ services: res.data });
+    });
+  }
+
+  addItem() {
+    var numItems = this.state.numItems
+    this.setState({numItems: ++numItems})
+  }
+
+  removeItem(num) {
+    console.log(num)
   }
 
   render() {
+    
+
     return (
       <div>
         <Typography variant="h5" component="span" color="secondary">
           <h5>Create an Estimate</h5>
         </Typography>
         <div>
-        <form autoComplete="off" id="create-form">
-          <TextField
-            name="name"
-            type="text"
-            color="secondary"
-            placeholder="Client Name"
-            onChange={this.handleChange}
-          />
-          <TextField
-            name="title"
-            type="text"
-            color="secondary"
-            placeholder="Title"
-            onChange={this.handleChange}
-          />
-          <TextField
-            name="email"
-            type="email"
-            color="secondary"
-            placeholder="Client Email"
-            onChange={this.handleChange}
-          />
-          <TextField
-            name="address"
-            type="text"
-            color="secondary"
-            placeholder="Client Street Address"
-            onChange={this.handleChange}
-          />
-          <TextField
-            name="cityState"
-            type="text"
-            color="secondary"
-            placeholder="Client City, State"
-            onChange={this.handleChange}
-          />
-          <TextField
-            name="zip"
-            type="text"
-            pattern="[0-9]{5}"
-            color="secondary"
-            placeholder="Client Zip Code"
-            onChange={this.handleChange}
-          />
-          <TextField
-            name="expiration"
-            type="text"
-            pattern="[0-9]"
-            color="secondary"
-            placeholder="Expires in x days"
-            onChange={this.handleChange}
-          />
-        </form>
+          <form autoComplete="off" id="create-form">
+            <TextField
+              name="name"
+              type="text"
+              color="secondary"
+              placeholder="Client Name"
+              onChange={this.handleChange}
+            />
+            <TextField
+              name="title"
+              type="text"
+              color="secondary"
+              placeholder="Title"
+              onChange={this.handleChange}
+            />
+            <TextField
+              name="email"
+              type="email"
+              color="secondary"
+              placeholder="Client Email"
+              onChange={this.handleChange}
+            />
+            <TextField
+              name="address"
+              type="text"
+              color="secondary"
+              placeholder="Client Street Address"
+              onChange={this.handleChange}
+            />
+            <TextField
+              name="cityState"
+              type="text"
+              color="secondary"
+              placeholder="Client City, State"
+              onChange={this.handleChange}
+            />
+            <TextField
+              name="zip"
+              type="text"
+              pattern="[0-9]{5}"
+              color="secondary"
+              placeholder="Client Zip Code"
+              onChange={this.handleChange}
+            />
+            <TextField
+              name="expiration"
+              type="text"
+              pattern="[0-9]"
+              color="secondary"
+              placeholder="Expires in x days"
+              onChange={this.handleChange}
+            />
+          </form>
         </div>
-        <form>
-          <select name="service-item">
-          {this.state.services ? this.state.services.map(item => (
-            <option value={item} key={item}>{item}</option>
-          )) : ""}
-          </select>
-          <TextField
-            name="item-description"
-            type="text"
-            color="secondary"
-            placeholder="Description"
-          />
-          <FormControlLabel
-          color='secondary'
-          value="tax"
-          control={<Checkbox color="secondary" />}
-          label="Tax"
-          labelPlacement="end"
-        />
-        </form>
+        <Button onClick={this.addItem}>Add Item</Button>
+        {this.state.itemsField}
       </div>
     );
   }
---
+++ client/src/components/privateComponents/estimates-admin/estimates-items-field.js
@@ -0,0 +1,82 @@
+import React from "react";
+import {
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Select,
+  MenuItem,
+  IconButton
+} from "@material-ui/core";
+import DeleteOutlinedIcon from "@material-ui/icons/DeleteOutlined";
+
+export default class ItemField extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      serviceItem: "",
+      itemDescription: "",
+      tax: false,
+      expense: false
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleCheckbox = this.handleCheckbox.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleCheckbox(event, isChecked) {
+    this.setState({ [event.target.name]: isChecked });
+  }
+
+  render() {
+    return (
+      <form>
+        <Select
+          onChange={this.handleChange}
+          value={this.state.serviceItem}
+          name="serviceItem"
+        >
+          {this.props.services
+            ? this.props.services.map(item => (
+                <MenuItem value={item} key={item}>
+                  {item}
+                </MenuItem>
+              ))
+            : ""}
+        </Select>
+        <TextField
+          name="itemDescription"
+          type="text"
+          color="secondary"
+          placeholder="Description"
+          onChange={this.handleChange}
+        />
+        <FormControlLabel
+          color="secondary"
+          className="unselectable"
+          name="tax"
+          control={<Checkbox color="secondary" />}
+          label="Tax"
+          labelPlacement="end"
+          onChange={this.handleCheckbox}
+        />
+        <FormControlLabel
+          color="secondary"
+          className="unselectable"
+          name="expense"
+          control={<Checkbox color="secondary" />}
+          label="Expense"
+          labelPlacement="end"
+          onChange={this.handleCheckbox}
+        />
+        <IconButton color="secondary" onClick={this.props.removeItem(this.props.num)}>
+          <DeleteOutlinedIcon />
+        </IconButton>
+      </form>
+    );
+  }
+}
