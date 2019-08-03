10089ce45f74c56839ff19b5937068c66aff88ed
Facundo Figueroa <facundof13@gmail.com>
2019-08-03 15:41:39 -0400

    contract finished

--- models/pdfgenerator.js
+++ models/pdfgenerator.js
@@ -17,16 +17,34 @@ function renderPdf(data, cb) {
   var PdfPrinter = require("pdfmake");
   var printer = new PdfPrinter(fonts);
 
-  var estimateOrInvoice = data.estimate.paid ? "Receipt" : data.estimate.invoice ? "Invoice" : "Estimate";
+  var estimateOrInvoice = data.estimate.paid
+    ? "Receipt"
+    : data.estimate.invoice
+    ? "Invoice"
+    : "Estimate";
   var items = [];
   var descriptions = [];
   var quantities = [];
   var amounts = [];
   var taxes = 0;
+  let paymentSchedule = [];
+
   var currencyFormatter = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD"
-  })
+  });
+
+  for (let j = 0; j < data.estimate.paymentSteps.length; j++) {
+    paymentSchedule.push(
+      `${data.estimate.paymentSteps[j].stepName} ${
+        data.estimate.paymentSteps[j].stepDescription
+      } ${currencyFormatter.format(
+        Number(data.estimate.paymentSteps[j].stepAmount)
+      )}\n`
+    );
+  }
+
+  // console.log(paymentSchedule);
 
   for (let i = 0; i < data.estimate.items.length; i++) {
     let num = data.estimate.items[i].amount;
@@ -39,9 +57,6 @@ function renderPdf(data, cb) {
 
   for (let i = 0; i < data.estimate.items.length; i++) {
     let numRows = Math.ceil(data.estimate.items[i].description.length / 49) - 1;
-    console.log(typeof(data.estimate.items[i].amount))
-    console.log(data.estimate.items[i].amount.replace(/\$/g, '').replace(/\.00+/, ''))
-
     let margin = 0;
     if (numRows > 0) {
       margin = numRows * 13;
@@ -69,7 +84,9 @@ function renderPdf(data, cb) {
       font: "RobotoMono"
     });
     amounts.push({
-      text: currencyFormatter.format(data.estimate.items[i].amount.replace(/\$/g, '').replace(/\.00+/, '')),
+      text: currencyFormatter.format(
+        data.estimate.items[i].amount.replace(/\$/g, "").replace(/\.00+/, "")
+      ),
       alignment: "right",
       fontSize: 10,
       height: "auto",
@@ -134,7 +151,7 @@ function renderPdf(data, cb) {
       },
       {
         text: data.estimate.paid ? "PAID" : "",
-        alignment:'center'
+        alignment: "center"
       },
       {
         table: {
@@ -219,37 +236,150 @@ function renderPdf(data, cb) {
   if (data.estimate.attachContract) {
     docDefinition.content.push(
       {
-      text:'EF Contractors LLC',
-      bold: true,
-      alignment:'center',
-      fontSize:'24',
-      pageBreak: 'before'
-    },
-    {
-      text:'404-409-3715', //TODO: Make phone number dynamic, store in db!
-      alignment:'center',
-      fontSize: 10
-    },
-    {
-      text:'\n\nCONTRACT AGREEMENT',
-      fontSize: 14,
-      alignment: 'center',
-      bold: true
-    },
-    {
-      text: ['\nDate ', prettifyDate(data.estimate.date)]
-    },
-    {
-      text: [`\n${data.client.name}`, `\n${data.client.email}`, `\n${data.client.address}`, `\n${data.client.cityState}`, `\n${data.client.zip}`]
-    },
-    {
-      text: ['\nJOB TITLE: ', data.estimate.title]
-    },
-    {
-      text: '\nArticle One: Contract Document'
-    }
-
-    )
+        text: "EF Contractors LLC",
+        bold: true,
+        alignment: "center",
+        fontSize: "24",
+        pageBreak: "before"
+      },
+      {
+        text: "404-409-3715", //TODO: Make phone number dynamic, store in db!
+        alignment: "center",
+        fontSize: 10
+      },
+      {
+        text: "\n\nCONTRACT AGREEMENT",
+        fontSize: 14,
+        alignment: "center",
+        bold: true
+      },
+      {
+        text: ["\nDate ", prettifyDate(data.estimate.date)]
+      },
+      {
+        text: [
+          `\n${data.client.name}`,
+          `\n${data.client.email}`,
+          `\n${data.client.address}`,
+          `\n${data.client.cityState}`,
+          `\n${data.client.zip}`
+        ]
+      },
+      {
+        text: ["\nJOB TITLE: ", data.estimate.title]
+      },
+      {
+        text: "\nArticle One: Contract Document"
+      },
+      {
+        text: `\nThese documents constitute an agreement between EF Contractors LLC hereinafter referred to as the "Contractor" and ${
+          data.client.name
+        } hereinafter referred to as the "Owner", to renovate the project located at ${
+          data.client.address
+        }, ${data.client.cityState} ${data.client.zip}`,
+        margin: [40, 0],
+        alignment: "left"
+      },
+      {
+        text:
+          "\nThe contract documents consists of this agreement, general conditions, construction documents, specifications, allowances, finish schedule, construction draw schedule, addenda issued prior to by both parties.",
+        margin: [40, 0],
+        alignment: "left"
+      },
+      {
+        text:
+          "\nThe Contractor shall provide all documents noted herein to the Owner. These contract documents represent the entire agreement of both parties and supersede any prior oral or written agreement."
+      },
+      {
+        text: "\n\nArticle Two: Duties of the Contractor"
+      },
+      {
+        text: [
+          "\nAll work shall be in concordance to the provisions of the plans and specification. All systems shall be in good working order.",
+          "\n\nAll work completed in a competent manner, and shall comply with all applicable national, state, and local building codes and laws.",
+          "\n\nAll individuals will perform their said work as outlined by law.",
+          "\n\nContractor shall remove all construction debris and leave the project in a broom clean conditions."
+        ],
+        alignment: "left",
+        margin: [40, 0]
+      },
+      {
+        text: "\n\nArticle Three: Owner"
+      },
+      {
+        text: [
+          "\nThe Owner shall communicate with subcontractors only through the Contractor.",
+          "\n\nThe Owner will not assume any liability or responsibility, nor control over or change of construction means, methods, techniques, sequences, procedures, or for safety precautions and programs in connection with the project since these are solely the Contractor's responsibility."
+        ],
+        alignment: "left",
+        margin: [40, 0]
+      },
+      {
+        text: "\n\nArticle Four: Change Order and Finish Schedules"
+      },
+      {
+        text: [
+          "\nA Change Order is any charge to the original contracted plans and/or specifications.",
+          "\n\nAll change orders to be administered through direct contact between the Owner and the Contractor.",
+          "\n\nAny additional time needed to complete change order is into consideration in the project completion date."
+        ],
+        alignment: "left",
+        margin: [40, 0]
+      },
+      {
+        text:
+          "\n\nAny delays or changes in finish selection schedules may delay the projected completion date. Contractor's Initials ____"
+      },
+      {
+        text: "\n\nArticle Five: Building & Specifications"
+      },
+      {
+        text: "\n" + data.estimate.contractSpecs,
+        alignment: "left",
+        margin: [40, 0]
+      },
+      {
+        text: "\n\nArticle Six: Progress Payments"
+      },
+      {
+        text: [
+          "\n\nThe owner will make payments to the contractor pursuant to the construction draw schedule as each phase of the construction schedule is satisfactorily complete",
+          "\n\nDraw payments are made payable to EF Contractors LLC"
+        ],
+        alignment: "left",
+        margin: [40, 0]
+      },
+      {
+        text: paymentSchedule.length > 0 ? "\nPayment schedule\n\n" : "",
+        alignment: "center"
+      },
+      {
+        text: paymentSchedule,
+        alignment: "center"
+      },
+      {
+        text: "\n\nArticle Seven: Warranty",
+        alignment: 'left'
+      },
+      {
+        text:
+          "\n\nAt the completion of the project, contractor shall execute an instrument to owner warranting the project for one year against defects in workmanship or materials utilized.",
+        alignment: "left",
+        margin: [40, 0]
+      },
+      {
+        text: [
+          "\n\n\n\n\n\n\n" +
+            "________________________________________" +
+            "\nErnesto Figueroa" +
+            "                                Date",
+          "\n\n\n\n\n\n\n" +
+            "________________________________________" +
+            `\n${data.client.name}` +
+            "                               Date"
+        ]
+      }
+    );
   }
 
   var options = {
