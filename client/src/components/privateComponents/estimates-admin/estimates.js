286b639b7208c5bac7cefd116e9ed6a8a17a1b7c
Facundo Figueroa <facundof13@gmail.com>
2019-08-15 02:48:55 -0400

    estimates tabs added

--- client/src/components/privateComponents/estimates-admin/estimates.js
+++ client/src/components/privateComponents/estimates-admin/estimates.js
@@ -1,36 +1,21 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import CreateEstimate from "./create-estimates";
-import { Button } from "@material-ui/core";
+import { Button, Tabs, Tab, AppBar } from "@material-ui/core";
 
 //components
 import EstimatesTable from "./estimates-table";
+import CreateEstimate from "./create-estimates";
 
 export default class Estimates extends React.Component {
   constructor() {
     super();
-    this.state = { showCreateEstimate: false, showTable: true };
+    this.state = { showCreateEstimate: false, showTable: true, value: "one" };
 
-    this.toggleCreate = this.toggleCreate.bind(this);
-    this.toggleTable = this.toggleTable.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  toggleCreate() {
-    if (!this.state.showCreateEstimate && this.state.showTable) {
-      this.setState({ showTable: false });
-    }
-    this.setState(prevState => ({
-      showCreateEstimate: !prevState.showCreateEstimate
-    }));
-  }
-
-  toggleTable() {
-    if (this.state.showCreateEstimate && !this.state.showTable) {
-      this.setState({ showCreateEstimate: false });
-    }
-    this.setState(prevState => ({
-      showTable: !prevState.showTable
-    }));
+  handleChange(e, value) {
+    this.setState({ value: value });
   }
 
   render() {
@@ -39,29 +24,19 @@ export default class Estimates extends React.Component {
         <Typography component="span" variant="h4" color="secondary">
           <h4>Manage Estimates</h4>
         </Typography>
-        <Button color="secondary" onClick={this.toggleCreate}>
-          {this.state.showCreateEstimate ? "Hide Create" : "Show Create"}
-        </Button>
-        <Button color="secondary" onClick={this.toggleTable}>
-          {this.state.showTable ? "Hide Table" : "Show Table"}
-        </Button>
+        <AppBar position="static" className="tabs-bar">
+          <Tabs
+            value={this.state.value}
+            onChange={this.handleChange}
+            centered={true}
+          >
+            <Tab value="one" label="Create Estimates" />
+            <Tab value="two" label="Manage Estimates" />
+          </Tabs>
+        </AppBar>
 
-        {/* TODO: Add materialUI tab components here */}
-        {this.state.showCreateEstimate ? (
-          <div id="showCreateEstimate">
-            <CreateEstimate />
-          </div>
-        ) : (
-          ""
-        )}
-
-        {this.state.showTable ? (
-          <div id="showEstimateTable">
-            <EstimatesTable />
-          </div>
-        ) : (
-          ""
-        )}
+        {this.state.value === "one" ? <CreateEstimate /> : ""}
+        {this.state.value === "two" ? <EstimatesTable /> : ""}
       </div>
     );
   }
--- client/src/components/privateComponents/settings/settings.js
+++ client/src/components/privateComponents/settings/settings.js
@@ -1,21 +1,15 @@
 import React from "react";
 import {
   Typography,
-  Divider,
   Tabs,
   Tab,
   AppBar,
-  TabPanel
 } from "@material-ui/core";
 import CompanyInformation from "./company-information";
 import ManageServices from "./manage-services";
 import InvoiceSettings from "./invoice-settings";
 
-const values = {
-  "COMPANY SETTINGS": "one",
-  "INVOICE SETTINGS": "two",
-  "MANAGE SERVICES": "three"
-};
+
 export default class Settings extends React.Component {
   constructor(props) {
     super(props);
@@ -47,19 +41,6 @@ export default class Settings extends React.Component {
         {this.state.value === "one" ? <CompanyInformation /> : ""}
         {this.state.value === "two" ? <InvoiceSettings /> : ""}
         {this.state.value === "three" ? <ManageServices /> : ""}
-
-        {/* 
-        <TabPanel value={this.state.value} index='one'>Company Settings</TabPanel>
-        <TabPanel value={this.state.value} index='one'>Invoice Settings</TabPanel>
-        <TabPanel value={this.state.value} index='one'>Manage Services</TabPanel> */}
-
-        {/* <Divider className='divider' />
-        <CompanyInformation />
-        <Divider className='divider' />
-        <InvoiceSettings />
-        <Divider className='divider' />
-        <ManageServices />
-        <Divider className='divider' /> */}
       </div>
     );
   }
