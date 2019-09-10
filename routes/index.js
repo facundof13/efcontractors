41fd373217e60bb516cbe67d6b454e11f3b8f3fb
Facundo Figueroa <facundof13@gmail.com>
2019-09-10 01:30:39 -0400

    heroku

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,16 @@ router.use(function(req, res, next) {
   next();
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'react-ui/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'react-ui/build', 'index.html'));
+  });
+}
+
+
 router.post(
   "/login",
   function(req, res, next) {
