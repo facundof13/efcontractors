f52c8e550e1c78911f83d086e66bf193d7b7b663
Facundo <facundof13@gmail.com>
2020-11-13 23:10:16 -0500

    added search

--- react-ui/src/components/privateComponents/estimates-admin/select-existing-client.js
+++ react-ui/src/components/privateComponents/estimates-admin/select-existing-client.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { MenuItem, FormControl, InputLabel, Select } from '@material-ui/core';
+import { MenuItem, FormControl, InputLabel, Select, TextField} from '@material-ui/core';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 export default class SelectExistingClient extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			selectedCustomer: []
+			selectedCustomer: null
 		};
 		this.handleChange = this.handleChange.bind(this);
 	}
 
-	handleChange(event) {
+	handleChange(event,value) {
+		event.preventDefault();
+		console.log(value)
 		this.setState(
 			{
-				selectedCustomer: event.target.value
+				selectedCustomer: value
 			},
 			() => this.props.update(this.state.selectedCustomer)
 		);
@@ -20,23 +23,18 @@ export default class SelectExistingClient extends React.Component {
 
 	render() {
 		let value = (
-			<form id='item-form'>
+			<form id='item-form' onSubmit={e => { e.preventDefault();}}>
 				<div className='login'>
 					<FormControl>
-						<InputLabel htmlFor='customer-select'>Client</InputLabel>
-						<Select
-							className='estimate-item-select-width'
-							id='item-select'
+						<Autocomplete
+							id="item-select"
+							options={this.props.customers}
 							onChange={this.handleChange}
-							value={this.state.selectedCustomer}
-							name='selectedCustomer'>
-							<MenuItem value=''>None</MenuItem>
-							{this.props.customers.map((customer) => (
-								<MenuItem value={customer} key={customer._id}>
-									{customer.name}
-								</MenuItem>
-							))}
-						</Select>
+							getOptionLabel={(customer) => customer.name}
+							getOptionSelected={(customer, value) => customer._id === value._id}
+							style={{ width: 300 }}
+							renderInput={(params) => <TextField {...params} label="Client" variant="standard" />}
+							/>
 					</FormControl>
 				</div>
 			</form>
